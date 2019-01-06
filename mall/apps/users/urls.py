765b3e0ab2610c93eb5709cc2a92fd725ed942a5
李赟辉 <xwp_fullstack@163.com>
2019-01-06 21:07:07 +0800

    用户名的校验

---
+++ mall/apps/users/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+   # url(r'^usernames/(?P<username>\w{5, 20})/count/$', views.RegisterUsernameCountAPIView.as_view(), name='usernamecount'),
+   url(r'^usernames/(?P<username>\w{5,20})/count/$', views.RegisterUsernameCountAPIView.as_view(),name='usernamecount'),
+
+]
--- mall/apps/users/views.py
+++ mall/apps/users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+# 验证用户名
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+# 请求方式GET    /users/usernames/(?P<username>\w{5, 20})/count/
+from users.models import User
+
+
+class RegisterUsernameCountAPIView(APIView):
+
+    def get(self, request, username):
+        # 通过用户名查询数据库该用户名的个数
+        count = User.objects.filter(username=username).count()
+        # 将数据组织返回给前端
+        context = {
+            'count': count,
+            'username': username
+        }
+        return Response(context)
+
