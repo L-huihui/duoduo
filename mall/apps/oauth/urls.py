1ebdf2a1c1cd2e3dad33096da1a712f02aa41e2a
李赟辉 <xwp_fullstack@163.com>
2019-01-10 16:29:57 +0800

    QQ授权openid的获取

--- mall/apps/oauth/urls.py
+++ mall/apps/oauth/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from .views import OAuthQQURLView
+from .views import OAuthQQURLView, OAuthQQUserAPIView
 urlpatterns = [
-    url(r'^qq/statues/$', OAuthQQURLView.as_view())
+    url(r'^qq/statues/$', OAuthQQURLView.as_view()),
+    url(r'^qq/users/$', OAuthQQUserAPIView.as_view())
 ]
--- mall/apps/oauth/views.py
+++ mall/apps/oauth/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 # 返回ＱＱ登录网址的视图
 # 请求方式：GET:/oquth/qq/status/?state=xxx
+from rest_framework import status
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -44,4 +45,36 @@ class OAuthQQURLView(APIView):
                         redirect_uri=settings.QQ_REDIRECT_URI,
                         state=state)
         login_url = oauth.get_qq_url()
-        return Response({'login_url':login_url })
+        return Response({'login_url': login_url})
+
+
+'''
+1, 用户同意授权登录的时候（用户扫码确认登录的时候），会返回一个code
+2, 我们用code换取ｔｏｋｅｎ
+3, 有了token 然后再换取openid
+'''
+'''
+前端接收到用户同意之后的code ,前段讲code发送给后端
+１，接收这个code数据
+2, 用code换ｔｏｋｅｎ
+３，用token换openid
+请求方式：　／oauth/qq/users/
+'''
+
+
+class OAuthQQUserAPIView(APIView):
+    def get(self, request):
+        # １，接收这个code数据
+        params = request.query_params
+        code = params.get('code')
+        # 2, 用code换ｔｏｋｅｎ
+        if code is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        oauth = OAuthQQ(client_id=settings.QQ_CLIENT_ID,
+                        client_secret=settings.QQ_CLIENT_SECRET,
+                        redirect_uri=settings.QQ_REDIRECT_URI,
+                        )
+        token = oauth.get_access_token(code)
+        # ３，用token换openid
+        openid = oauth.get_open_id(token)
+        pass
